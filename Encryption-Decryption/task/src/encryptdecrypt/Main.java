4eec6d7aedfd6f20b2f0615fd754e1d1b301941d
EVA <seny_ussur@mail.ru>
2019-12-04 02:43:15 +1000

    Stage 5

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,11 +1,13 @@
 package encryptdecrypt;
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.TreeMap;
+import java.io.FileWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.spec.ECField;
+import java.util.*;
 
 public class Main {
     static  String alphabet="abcdefghijklmnopqrstuvwxyz";
+    static Map<String,String> defaultArgs=Map.of("-mode","enc","-key","0","-data","");
 
 
     public static String encriptOffset(String text, int offset){
@@ -37,33 +39,51 @@ public class Main {
             throw new IllegalArgumentException("Неверная комманда");
         }
     }
-    public static String getMode(String[] args){
-        int index=Arrays.asList(args).indexOf("-mode");
-        if (index==-1 || index==args.length-1) return "enc";
-        String mode=args[index+1];
-        if (mode.startsWith("-")) return "enc";
-        return mode;
-    }
-    public static String getData(String[] args){
-        int index=Arrays.asList(args).indexOf("-data");
-        if (index==-1 || index==args.length-1) return "";
-        String data=args[index+1];
-        if (data.startsWith("-")) return "";
-        return data;
-    }
-    public static int getOffset(String[] args){
-        int key=0;
-        int index=Arrays.asList(args).indexOf("-key");
-        if (index==-1 || index==args.length-1) return key;
-        try{
-             key=Integer.parseInt(args[index+1]);
-        }
-        catch (Exception e){
-        }
-        return key;
+
+
+    public static String getArg(List<String> args,String arg){
+        String result=defaultArgs.get(arg);
+        int index=args.indexOf(arg);
+        if (index==-1 || index==args.size()-1) return result;
+        result=args.get(index+1);
+        return result;
     }
 
+    public static String inputData(List<String> args){
+        if (args.contains("-data")){
+            return getArg(args,"-data");
+        }
+        else if(args.contains("-in")){
+             try {
+                return new String(Files.readAllBytes(Paths.get(getArg(args, "-in"))));
+            }
+            catch (Exception e){
+                System.out.println("Error");
+            }
+        }
+        return defaultArgs.get("-data");
+    }
+
+    public static void outData(List<String> args,String data){
+        if (args.contains("-out")){
+            try(FileWriter writer=new FileWriter(getArg(args,"-out"))){
+                writer.write(data);
+            }
+            catch (Exception e){
+                System.out.println("Error");
+            }
+        }
+        System.out.println(data);
+    }
+
+
     public static void main(String[] args) {
-        System.out.println(doOffset(getMode(args),getData(args),getOffset(args)));
+        //args=new String[]{"-mode","enc", "-in","road_to_treasure.txt","-out","protected.txt","-key","5"};
+        List<String> list= Arrays.asList(args);
+        String inData=inputData(list);
+        String command=getArg(list,"-mode");
+        int offset=Integer.parseInt(getArg(list,"-key"));
+        String outData=doOffset(command,inData,offset);
+        outData(list,outData);
     }
 }

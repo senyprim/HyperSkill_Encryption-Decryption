0cbc4331f5c159dfd8fc0a23e88f1beba24320ad
EVA <seny_ussur@mail.ru>
2019-12-04 00:57:39 +1000

    Stage 4

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,7 +1,12 @@
 package encryptdecrypt;
+import java.util.Arrays;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.TreeMap;
+
 public class Main {
     static  String alphabet="abcdefghijklmnopqrstuvwxyz";
+
 
     public static String encriptOffset(String text, int offset){
         char[] chars=new char[text.length()];
@@ -32,10 +37,33 @@ public class Main {
             throw new IllegalArgumentException("Неверная комманда");
         }
     }
+    public static String getMode(String[] args){
+        int index=Arrays.asList(args).indexOf("-mode");
+        if (index==-1 || index==args.length-1) return "enc";
+        String mode=args[index+1];
+        if (mode.startsWith("-")) return "enc";
+        return mode;
+    }
+    public static String getData(String[] args){
+        int index=Arrays.asList(args).indexOf("-data");
+        if (index==-1 || index==args.length-1) return "";
+        String data=args[index+1];
+        if (data.startsWith("-")) return "";
+        return data;
+    }
+    public static int getOffset(String[] args){
+        int key=0;
+        int index=Arrays.asList(args).indexOf("-key");
+        if (index==-1 || index==args.length-1) return key;
+        try{
+             key=Integer.parseInt(args[index+1]);
+        }
+        catch (Exception e){
+        }
+        return key;
+    }
+
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-
-        System.out.println(doOffset(scanner.nextLine(),scanner.nextLine(),scanner.nextInt()));
-
+        System.out.println(doOffset(getMode(args),getData(args),getOffset(args)));
     }
 }

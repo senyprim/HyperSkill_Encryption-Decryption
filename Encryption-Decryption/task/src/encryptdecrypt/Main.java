9b3b4616bab271d6aa1ffd788c8da8bd2da1871c
EVA <seny_ussur@mail.ru>
2019-12-04 00:29:35 +1000

    Stage 3

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -8,18 +8,34 @@ public class Main {
 
         for(int index=0;index<text.length();index++)
         {
-            char ch=text.charAt(index);
-            if (Character.isLetter(ch)){
-                ch=(char) ('a'+(ch-'a'+offset)%alphabet.length());
-            }
-            chars[index]=ch;
+            chars[index]=(char)(text.charAt(index)+offset);
         }
         return new String(chars);
     }
+    public static String decriptOffset(String text, int offset){
+        char[] chars=new char[text.length()];
+
+        for(int index=0;index<text.length();index++)
+        {
+            chars[index]=(char)(text.charAt(index)-offset);
+        }
+        return new String(chars);
+    }
+    public static String doOffset(String command,String text,int offset){
+        if (command.equals("enc")){
+            return  encriptOffset(text,offset);
+        }
+        else if (command.equals("dec")){
+            return  decriptOffset(text,offset);
+        }
+        else {
+            throw new IllegalArgumentException("Неверная комманда");
+        }
+    }
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
-        String text=scanner.nextLine();
-        int offset=Integer.parseInt(scanner.nextLine());
-        System.out.println(encriptOffset(text,offset));
+
+        System.out.println(doOffset(scanner.nextLine(),scanner.nextLine(),scanner.nextInt()));
+
     }
 }
